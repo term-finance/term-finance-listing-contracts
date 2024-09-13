61d1aee9cc3391b4a40b5c8fe650644a3af7bc87
Robert Chu <robert@termfinance.io>
2024-09-13 11:16:09 -0700

    Adds back missing dependencies.

--- certora/harness/TermListingContractHarness.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier: CC-BY-NC-ND-4.0
-pragma solidity ^0.8.20;
-
-import "../../src/TermListingContract.sol";
-
-contract TermListingContractHarness is
-    TermListingContract
-{
-}
